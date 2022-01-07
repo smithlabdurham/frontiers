fbe56dcd581a22437ca2261dc33d447b3a1d4777
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-01-07 12:12:06 +0000

    DOI expander

---
+++ assets/js/doi.js
@@ -0,0 +1,16 @@
+$(window).on('load', function(){
+  // Display info
+  var Cite = require('citation-js');
+  
+  dois = document.getElementsByTagName('doi')
+  dois.forEach(function (ref) {
+    let doi = ref.getAttribute('doi');
+    bib = document.createElement('a');
+    bib.href = "https://dx.doi.org/" + doi;
+    bib.classList.add("doi-ref");
+    bib.innerHTML = new Cite(doi).format('bibliography', {
+        format: 'html', template: 'apa', lang: 'en-GB'
+      });
+    ref.appendChild(bib);
+  });
+});
