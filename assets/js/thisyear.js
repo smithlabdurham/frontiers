5e142f4f64ab50c1047a19ab70102b282a8a886b
SMITH <pjjg18@durham.ac.uk>
2022-03-10 09:12:17 +0000

    Link tog roups

--- assets/js/thisyear.js
+++ assets/js/thisyear.js
@@ -2,24 +2,25 @@ firstClass = new Date("2022-01-13");
 wenlockDeadline = new Date(firstClass.valueOf());
 wenlockDeadline.setDate(wenlockDeadline.getDate() + 72 + 17);
 
-days = document.querySelectorAll('[day]');
+days = document.querySelectorAll("[day]");
 for (var i = 0; i != days.length; i++) {
   dateI = new Date(firstClass.valueOf());
-  dateI.setDate(dateI.getDate() + parseInt(days[i].getAttribute('day')));
+  dateI.setDate(dateI.getDate() + parseInt(days[i].getAttribute("day")));
   days[i].innerHTML = dateI.toDateString();
 }
 
-weeks = document.querySelectorAll('[week]');
+weeks = document.querySelectorAll("[week]");
 for (var i = 0; i != weeks.length; i++) {
   dateI = new Date(firstClass.valueOf());
-  dateI.setDate(dateI.getDate() + ((parseInt(weeks[i].getAttribute('week')) - 1) * 7));
+  dateI.setDate(dateI.getDate() + ((parseInt(weeks[i].getAttribute("week")) - 1) * 7));
   weeks[i].innerHTML = dateI.toDateString();
 }
 
 const links = {
-  'discussionBoard': 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_685527_1?view=discussions&courseId=_5402_1',
-  'blackboard': 'https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline',
-  'readingList': 'https://rl.talis.com/3/durham/lists/719102AD-4B96-6917-9F03-5537497D5654.html',
+  "discussionBoard": "https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline/discussion/_685527_1?view=discussions&courseId=_5402_1",
+  "blackboard": "https://blackboard.durham.ac.uk/ultra/courses/_5402_1/outline",
+  "groupList": "https://docs.google.com/spreadsheets/d/1k1-IIILRZWNH5PuNZLF4YSzke9eNJ_AiO-T4f64lEbE/edit?usp=sharing",
+  "readingList": "https://rl.talis.com/3/durham/lists/719102AD-4B96-6917-9F03-5537497D5654.html",
 };
 
 for (const [aClass, aHref] of Object.entries(links)) {
